dc5fb8264974f150701acb7d7e9a6f958a023dcd
nbilledo <nbilledo@ucsc.edu>
2025-10-14 04:23:36 +0000

    Created non-interactive UI consisting of header, canvas, and three buttons (Step One Complete).
    Specifics:
    - Created canvas and button object well as a main-container class for the UI in the style.css file
    - Added a font through HTML file
    - Implemented canvas, clear, redo, and undo buttons in main.ts innerHTML
    - Removed paperclip

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+<div class="main-container">
+  <h1>WHITEBOARD<h1>
+  <canvas id="Canvas" width = "256" height="256"></canvas>
+  </br>
+  <button id="clear">CLEAR</button>
+  <button id="redo" style="background-color: #f8921eff;">REDO</button>
+  <button id="undo" style="background-color: #f72314ff;">UNDO</button>
+  </div>
 `;
